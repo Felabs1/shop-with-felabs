8b607f44f30ca577e84f4da30bdaa8e0b39bfa43
Felabs1 <felixawere01@gmail.com>
2022-06-21 17:25:15 +0300

    start ecommerce api | create user collection

---
+++ ecommerceapi/routes/auth.js
@@ -0,0 +1,45 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+	const newUser = new User({
+		username: req.body.username,
+		email: req.body.email,
+		password: CryptoJS.AES.encrypt(
+			req.body.password,
+			process.env.PASS_SEC
+		).toString(),
+	});
+	try {
+		const savedUser = await newUser.save();
+		res.status(201).json(savedUser);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		!user && res.status(401).json("wrong credentials");
+
+		const hashedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.PASS_SEC
+		);
+		const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+		OriginalPassword !== req.body.password &&
+			res.status(401).json("wrong credentials");
+
+		const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SEC, {expiresIn: "3d"})
+
+		const { password, ...others } = user._doc;
+		res.status(200).json({...others, accessToken});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+module.exports = router;
---
+++ ecommerceapi/routes/product.js
@@ -0,0 +1,105 @@
+const Product = require("../models/Product");
+const {
+	verifyToken,
+	verifyTokenAndAuthorization,
+	verifyTokenAndAdmin,
+} = require("./verifyToken");
+const router = require("express").Router();
+
+// create
+
+router.post("/", verifyTokenAndAdmin , async (req, res) => {
+	const newProduct = new Product(req.body);
+	try {
+		const savedProduct = await newProduct.save();
+		res.status(200).json(savedProduct);
+	}catch(err){
+		res.status(500).json(err);
+	}
+})
+
+
+
+/*
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+	if (req.body.password) {
+		req.body.password = CryptoJS.AES.encrypt(
+			req.body.password,
+			process.env.PASS_SEC
+		).toString();
+	}
+
+	try {
+		const updatedUser = await User.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.status(200).json(updatedUser);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+	try {
+		await User.findByIdAndDelete(req.params.id);
+		res.status(200).json("user has been deleted");
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// get
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const { password, ...others } = user._doc;
+		res.status(200).json(others);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// get all users
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+	const query = req.query.new;
+	try {
+		const users = query
+			? await User.find().sort({ _id: -1 }).limit(1)
+			: await User.find();
+		res.status(200).json(users);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// get user stats
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+	const date = new Date();
+	const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+	try {
+		const data = await User.aggregate([
+			{ $match: { createdAt: { $gte: lastYear } } },
+			{
+				$project: {
+					month: { $month: "$createdAt" },
+				},
+			},
+			{
+				$group: {
+					_id: "$month",
+					total: { $sum: 1 },
+				},
+			},
+		]);
+		res.status(200).json(data)
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});*/
+module.exports = router;
